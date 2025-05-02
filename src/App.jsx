47d644502298950ad007f3ea081824fec942330d
alone-02 <santoshbhaskal2127@gmail.com>
2025-05-02 13:45:47 +0530

    remove build

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
               <Route path="/forgot_password" element={<ForgotPassword />} />
             </Routes>
             <Footer />
+            
           </div>
         </IdolProvider>
       </AuthProvider>
