879e24106f978a678c2359d82872acd98d9b7497
alone-02 <santoshbhaskal2127@gmail.com>
2025-05-02 13:49:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
               <Route path="/forgot_password" element={<ForgotPassword />} />
             </Routes>
             <Footer />
-            
+
           </div>
         </IdolProvider>
       </AuthProvider>
